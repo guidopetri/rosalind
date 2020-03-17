eda391a6574d6917bda47f7f4d5b517de1fe01dc
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2020-03-17 01:31:15 -0400

    fibonacci via list

--- algorithms/FIBO.py
+++ algorithms/FIBO.py
@@ -2,11 +2,16 @@
 
 
 def fibo(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    return fibo(n - 1) + fibo(n - 2)
+    # early exit
+    if n < 2:
+        return n
+
+    res = [0, 1]
+
+    for i in range(2, n + 1):
+        res.append(res[i - 1] + res[i - 2])
+
+    return res[n]
 
 
 with open('rosalind_fibo.txt', 'r') as f:
